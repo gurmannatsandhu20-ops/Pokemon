475c71f97b91cff7842f385d374eff666fca29ff
gurmannatsandhu20-ops <gurmannatsandhu20@gmail.com>
2025-12-12 16:29:36 -0700

    ex-1

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int main() {
+string player_name;
+cin >> player_name;
+cout << "Enter your name: ";
 
+cout << "Great Start,"<< player_name << "looks like you understand main();" <<endl;
     return 0;
 }
